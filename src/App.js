f893645ce8884309719e604599ee79631b5a91c1
Hyunjin Yun <sayconclusion@gmail.com>
2018-12-26 13:44:06 +0900

    Author: hjyun\r\nUse Error Boundaries

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import React, { Component, Fragment } from 'react';
 import { createPortal } from 'react-dom';
 import './App.css';
 
+class ErrorMaker extends Component {
+  state = {
+    friends: ["jisu", "flynn", "daal", "kneeplayer"]
+  };
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        friends: undefined
+      });
+    }, 2000);
+  }
+  render() {
+    const { friends } = this.state;
+    return friends.map(friend => ` ${friend}`)
+  }
+}
+
 class Portals extends Component {
   render() {
     return createPortal(<Message />, document.getElementById("touchme"));
@@ -22,12 +39,25 @@ class ReturnTypes extends Component {
   }
 }
 
+const ErrorFallback = () => "Sorry something went wrong";
+
 class App extends Component {
+  state = {
+    hasError: false
+  }
+  componentDidCatch = (error, info) => {
+    console.log(`catched ${error} the info i have is ${JSON.stringify(info)}`);
+    this.setState({
+      hasError: true
+    })
+  }
   render() {
+    const { hasError } = this.state;
     return (
       <Fragment>
         <ReturnTypes />
         <Portals />
+        {hasError ? <ErrorFallback /> : <ErrorMaker />}
       </Fragment>
         
     )
