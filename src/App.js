84ee877435e650a02cfe3c1f1ff112e01ff2892f
Hyunjin Yun <sayconclusion@gmail.com>
2018-12-26 13:01:52 +0900

    Author: hjyun\r\nUse Fragment

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+
+class ReturnTypes extends Component {
+  render() {
+    return (
+      <Fragment>
+        <header>Header</header>
+        <div>Body</div>
+        <footer>Footer</footer>
+      </Fragment>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+        <ReturnTypes />
+    )
   }
 }
 
